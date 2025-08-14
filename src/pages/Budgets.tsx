583acac54fa98bc6f8fb89baba7ed5b039370d34
simonexlue <simonexlue@gmail.com>
2025-08-13 19:09:51 -0700

    .

--- src/pages/Budgets.tsx
+++ src/pages/Budgets.tsx
@@ -6,8 +6,13 @@ const Budgets: React.FC = () => {
         <div className="budgets-page">
             <div className="header">
                 <h1>Budgets</h1>
-
+                <div className="header-buttons">
+                    <button className="add-budget-button">+ New Budget</button>
+                    <button className="filter-budgets-button">F</button>
+                    <button className="extra-button">E</button>
+                </div>
             </div>
+            <hr className="budgets-divider" />
         </div>
     );
 }
